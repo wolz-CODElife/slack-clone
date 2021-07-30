554f2149469e3b83544c8ba49eb92d9f793f95ab
wolz-CODElife <joeladewole3@gmail.com>
2021-07-30 18:16:16 +0100

    Working on header-component

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import './Header.css'
-import Avatar from '@material'
+import { Avatar } from '@material-ui/core'
+import AccessTimeIcon from '@material-ui/icons/AccessTime'
+import SearchIcon from '@material-ui/icons/Search'
 
 const Header = () => {
     return (
         <div className="header">
             <div className="header_left">
-                {/*  */}
+                <Avatar className="header_avatar" alt='Joe Adewolz' src='' />
+                <AccessTimeIcon />
             </div>
-            <div className="header_search"></div>
+            <div className="header_search">
+                <Search />
+            </div>
             <div className="header_right"></div>
         </div>
     )
