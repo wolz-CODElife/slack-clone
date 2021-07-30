024327a2422dce524c72a8b6c3caa63ceeee9ccf
wolz-CODElife <joeladewole3@gmail.com>
2021-07-30 06:05:33 +0100

    Initiating slack-clone

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+import Avatar from '@material'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header_left">
+                {/*  */}
+            </div>
+            <div className="header_search"></div>
+            <div className="header_right"></div>
+        </div>
+    )
+}
+
+export default Header
